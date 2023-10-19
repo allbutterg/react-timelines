76dbd46d74715684e98d99a44538c49abd9f1b93
Gauri Kangai <gauri.kangai@outlook.com>
2023-10-19 01:30:22 +0100

    try replacing toggle icon with brand icons

--- src/components/Sidebar/TrackKeys/TrackKey.jsx
+++ src/components/Sidebar/TrackKeys/TrackKey.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Icons } from '@bbc/dwc-icons';
 
 import TrackKeys from '.'
 
@@ -24,14 +25,15 @@ const TrackKey = ({ track, toggleOpen, clickTrackButton }) => {
     <li className="rt-track-key">
       <div className="rt-track-key__entry">
         {isExpandable && (
-          <button
-            title="Expand track"
-            className={`rt-track-key__toggle ${isOpen ? 'rt-track-key__toggle--close' : 'rt-track-key__toggle--open'}`}
-            onClick={() => toggleOpen(track)}
-            type="button"
-          >
-            {isOpen ? 'Close' : 'Open'}
-          </button>
+<Icons set="brands" name={title.toLowerCase()} />
+          // <button
+          //   title="Expand track"
+          //   className={`rt-track-key__toggle ${isOpen ? 'rt-track-key__toggle--close' : 'rt-track-key__toggle--open'}`}
+          //   onClick={() => toggleOpen(track)}
+          //   type="button"
+          // >
+          //   {isOpen ? 'Close' : 'Open'}
+          // </button>
         )}
         <span className="rt-track-key__title">{title}</span>
         {buildSideComponent()}
